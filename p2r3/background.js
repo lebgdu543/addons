54b57a8031c527a7304d68b37dc1185cf11b4954
Zwaygo <103336695+lebgdu543@users.noreply.github.com>
2026-03-07 21:46:41 +0100

    Comment out option to always open a new tab
    Comment out the option to always open a new tab when the extension icon is clicked.

--- p2r3/background.js
+++ p2r3/background.js
@@ -5,8 +5,9 @@ const TARGET_URL = "https://p2r3.github.io/convert/";
 
 // Listen for the extension icon click
 chrome.action.onClicked.addListener((tab) => {
-  // Option 1: Always open a new tab
+  /* // Option 1: Always open a new tab
   chrome.tabs.create({ url: TARGET_URL });
+  */
 
 // Option 2 (Advanced): Check if the tab is already open and focus it instead
   chrome.tabs.query({}, (tabs) => {
